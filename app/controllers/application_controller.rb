46c92249c1e29c13f69e108b74f6e4d4480db023
Nick McDaniel <nickamcd@gmail.com>
2021-05-24 10:55:11 -0700

    Can pull list of todays games from MLB stats api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,2 @@
 class ApplicationController < ActionController::Base
-  @client = MLBStatsAPI::Client.new
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,22 @@
 class HomeController < ApplicationController
+  require 'mlb_stats_api'
+  require 'json'
+
+
   def index
-    
+    # Establish connection to stats api
+    @client = MLBStatsAPI::Client.new
+
+    # Get the list of schedule of games for date range of only today
+    @schedule = @client.schedule({sport_id: 1, date: Date.today})
+
+    # Pull out Dates from schedule
+    @dates = @schedule['dates']
+
+    # Get today from result
+    @date = @dates[0]
+
+    # Get games from today
+    @games = @date['games']
   end
 end
