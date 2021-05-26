b7adbcd30d1ac2c72bae33914ec1d8c19d0355fc
Nick McDaniel <nickamcd@gmail.com>
2021-05-26 09:52:36 -0700

    Got rid of unneccessary API calls that were slowing down the rendering of partials

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,22 +10,25 @@ class HomeController < ApplicationController
     @schedule = @client.schedule({sport_id: 1, date: Date.today})
 
     # Pull out games from dates[0] (today)
-    @games_hash = @schedule['dates'][0]['games']
+    @games = @schedule['dates'][0]['games']
 
     # Array to hold game primary keys
-    @games = []
-
+    @gamesPK = []
     # Array to hold game date/start time
     @game_dates = []
-
+    # Array to hold pregame/postgame status if game is not currently
+    # being played
     @game_statuses = []
+    # Array to hold teams participarting in
+    @game_teams = []
 
     # Push primary key of games into array to use for API Queries
     # Also push datetime of game for pregame/ingame status
-    @games_hash.each do |game|
-      @games.push(game['gamePk'])
+    @games.each do |game|
+      @gamesPK.push(game['gamePk'])
       @game_dates.push(game['gameDate'])
       @game_statuses.push(game['status']['abstractGameState'])
+      @game_teams.push(game['teams'])
     end
   end
 
